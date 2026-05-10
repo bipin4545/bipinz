c6af094fba4826706269566d6d1efa5a6abd8719
ainexfinder <bipintechai@gmail.com>
2026-05-10 14:42:51 +0530

    Reduce logo size on mobile

--- components/LogoImage.tsx
+++ components/LogoImage.tsx
@@ -16,7 +16,7 @@ export function LogoImage() {
       width={120}
       height={36}
       sizes="160px"
-      className="h-10 w-auto max-w-[140px] object-contain sm:h-11 sm:max-w-[160px]"
+      className="h-7 w-auto max-w-[100px] object-contain sm:h-11 sm:max-w-[160px]"
       priority
     />
   );
